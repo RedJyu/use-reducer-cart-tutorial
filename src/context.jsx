f0412fb679dc22620f9ddd97193466bd7c0ee163
RedJyu <piotr.stezycki@protonmai.com>
2023-11-06 14:32:32 +0100

    final price/item number

--- src/context.jsx
+++ src/context.jsx
@@ -1,6 +1,7 @@
 import { useContext, useReducer, useEffect, createContext } from 'react';
 import reducer from './reducer';
 import cartItems from './data';
+import { getTotals } from './utils';
 
 import {
   CLEAR_CART,
@@ -20,6 +21,7 @@ const initialState = {
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { totalAmount, totalCost } = getTotals(state.cart);
 
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
@@ -37,7 +39,15 @@ export const AppProvider = ({ children }) => {
 
   return (
     <AppContext.Provider
-      value={{ ...state, clearCart, removeItem, increase, decrease }}
+      value={{
+        ...state,
+        clearCart,
+        removeItem,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
     >
       {children}
     </AppContext.Provider>
